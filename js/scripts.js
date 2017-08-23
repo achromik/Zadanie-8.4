d9ed6f0d76cb552a8bd85d99b888e770d178511c
Aleksander Chromik <achromik@gmail.com>
2017-08-23 16:50:22 +0200

    Excercise 8.4 - JS Comments - Finished

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/* This is multi-line comment or block comment 
+in JS-file
+var name = "alex";
+
+Block comments are often used for formal documentation.
+ */
+
+var name = prompt('Hi, my name is Alex! \nWhat is your name?');
+
+// alert('Hello, ' + name + '! Welcome to Excercise 8.4! Nice to see you again!');
+
+// This will be ignored by JavaScript and line below too.
+// var name = "John Rambo";
+
+console.log('Hello, ' + name + '! Welcome to Excercise 8.4! Nice to see you again!');
